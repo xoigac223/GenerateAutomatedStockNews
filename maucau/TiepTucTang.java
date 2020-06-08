31a5ee6d1fd3127fe8d56d22c41be49b3394e671
buncha123 <66629482+buncha123@users.noreply.github.com>
2020-06-08 22:06:23 +0700

    Add files via upload

---
+++ maucau/TiepTucTang.java
@@ -0,0 +1,53 @@
+package maucau;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import stockmarket.BigStock;
+
+public class TiepTucTang extends MauCau {
+	private static ArrayList<String> dauCau = new ArrayList<String>();
+	private static ArrayList<String> giuaCau1 = new ArrayList<String>();
+	private static ArrayList<String> giuaCau2 = new ArrayList<String>();
+	
+	private void maucau() {
+		dauCau.add("Tiếp đà tăng của phiên sáng, ");
+		dauCau.add("Tiếp đà tăng của phiên sáng nay, ");
+		dauCau.add("Tiếp đà tăng của phiên giao dịch sáng nay, ");
+		dauCau.add("Nối dài sắc xanh từ phiên sáng, ");
+		dauCau.add("Nối dài sắc xanh của phiên giao dịch sáng nay, ");
+		
+		giuaCau1.add(" tiếp tục tăng ");
+		giuaCau1.add(" tăng thêm ");
+		giuaCau1.add(" cộng thêm ");
+		
+		giuaCau2.add(" lên ");
+		giuaCau2.add(" đạt ");
+		giuaCau2.add(" đạt mốc ");
+	}
+	
+	@Override
+	public String sinhCau (BigStock stock) {
+		String cau;
+		StringBuffer sb = new StringBuffer();
+		String symbol = stock.getSymbol();
+		double delta = stock.getGiaDauPhienChieu() - stock.getGiaChotPhienSang();
+		int cachviet = new Random().nextInt(2);
+		
+		String daucau = dauCau.get(new Random().nextInt(dauCau.size()));
+		String giuacau1 = giuaCau1.get(new Random().nextInt(giuaCau1.size()));
+		String giuacau2 = giuaCau2.get(new Random().nextInt(giuaCau2.size()));
+		
+		sb.append(daucau); sb.append(symbol);
+		if (cachviet == 0) {
+			sb.append(giuacau1); sb.append(delta); sb.append(" điểm,");
+			sb.append(giuacau2); sb.append(stock.getGiaDauPhienChieu()); sb.append(" điểm.");
+		} else {
+			sb.append(giuacau2); sb.append(stock.getGiaDauPhienChieu()); sb.append(" điểm,");
+			sb.append(giuacau1); sb.append(delta); sb.append(" điểm.");
+		}
+		
+		cau = sb.toString();
+		return cau;
+	}
+}
