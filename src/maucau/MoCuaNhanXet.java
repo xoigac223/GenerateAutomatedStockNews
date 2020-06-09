080e21d6e0daacb1c9259e2e49802963f48636f5
trado006 <66631447+trado006@users.noreply.github.com>
2020-06-09 21:29:36 +0700

    Add files via upload

---
+++ src/maucau/MoCuaNhanXet.java
@@ -0,0 +1,76 @@
+package maucau;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import stockmarket.BigStock;
+
+public class MoCuaNhanXet implements MauCau{
+	private static List<String> dauCauTichCuc;	
+	private static List<String> giuaCauTang; 
+	private static List<String> dauCauTieuCuc; 
+	private static List<String> giuaCauGiam; 
+	private static List<String> dauCauOnDinh;
+	private BigStock stock;
+	
+	public MoCuaNhanXet(BigStock stock) {
+		this.stock = stock;
+		dauCauTichCuc = new ArrayList<String>();
+		giuaCauTang = new ArrayList<String>();
+		dauCauTieuCuc = new ArrayList<String>();
+		giuaCauGiam = new ArrayList<String>();
+		
+		dauCauTichCuc.add("Sắc xanh của phiên cuối tuần trước tiếp tục được duy trì trên sàn");
+		
+		dauCauTieuCuc.add("Sắc đỏ của phiên cuối tuần trước tiếp tục được duy trì trên sàn");
+		
+		dauCauOnDinh.add("Mở đầu phiên giao dịch, cổ phiếu nhận tín hiệu trái ngược với tuần trước cụ thể ");
+		
+		giuaCauTang.add(" tăng thêm ");
+		giuaCauTang.add(" tiếp tục tín hiệu lạc quan khi nhích thêm ");
+		giuaCauTang.add(" cộng thêm ");
+		
+		giuaCauGiam.add(" điều chỉnh và giảm ");
+		giuaCauGiam.add("giảm điểm");
+		giuaCauGiam.add("tụt điểm");
+
+	}
+	
+	public String sinhCau() {
+		String moCuaNhanXet = null;
+		StringBuffer sb = new StringBuffer();
+		String dauCau;
+		String giuaCau;
+		String symbol = stock.getSymbol();
+		double giaMoCuaHomNay = stock.getGiaMoCua();
+		double giaDongCuaHomTruoc = stock.history(-1).getGiaDongCua();
+		double chenhLechHomNay = giaMoCuaHomNay - giaDongCuaHomTruoc;
+		
+		
+		double giaMoCuaTuanTruoc = stock.history(-7).getGiaMoCua();
+		double giaDongCuaTuanTruoc = stock.history(-8).getGiaDongCua();
+		double chenhLechTuanTruoc = giaMoCuaTuanTruoc - giaDongCuaTuanTruoc;
+		
+		
+		if ((chenhLechHomNay>0)&&(chenhLechTuanTruoc>0)) {
+			dauCau = dauCauTichCuc.get(new Random().nextInt(dauCauTichCuc.size()));
+		}else{
+		if((chenhLechHomNay<0)&&(chenhLechTuanTruoc<0)){
+			dauCau = dauCauTieuCuc.get(new Random().nextInt(dauCauTieuCuc.size()));
+		}else{
+			dauCau = dauCauOnDinh.get(new Random().nextInt(dauCauOnDinh.size()));
+		}
+		}
+		if (chenhLechHomNay >= 0) {
+			giuaCau = giuaCauTang.get(new Random().nextInt(giuaCauTang.size()));
+		} else{
+			giuaCau = giuaCauGiam.get(new Random().nextInt(giuaCauGiam.size()));
+			chenhLechHomNay = -chenhLechHomNay;
+		}
+		sb.append(dauCau); sb.append(symbol); sb.append("hôm nay ");sb.append(giaMoCuaHomNay);sb.append(giuaCau);sb.append("với độ tăng");
+		sb.append(String.format("%.3g", chenhLechHomNay)); sb.append("điểm");sb.append(".");
+		moCuaNhanXet = sb.toString();
+		return moCuaNhanXet;
+	}
+}
